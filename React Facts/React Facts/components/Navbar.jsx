05bba6d468d095500ab4d495c68e00ea624957a1
frankie-powers <franklin.power21@gmaul.com>
2024-09-25 11:57:32 +0100

    initial commit

---
+++ React Facts/React Facts/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="nav-container">
+      <img src="./src/assets/react.svg" alt="react logo" className="nav-logo" />
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </div>
+  );
+}
--- Vite+React/vite-project-todo/src/App.jsx
+++ Vite+React/vite-project-todo/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [todos, setTodos] = useState(["cook", "sleep", "code"]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container mx-auto p-8">
+      <h1 className="text-4xl border-b-4 border-black">Todo</h1>
+      <ul>
+        <li>{todos[0]}</li>
+        <li>{todos[1]}</li>
+        <li>{todos[2]}</li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
